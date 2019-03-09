007d1cfbe908a91f3b45a2d9554255b781699331
ZumaDigital <zumadigitalmx@gmail.com>
2019-03-08 19:49:42 -0600

    Component for Header

---
+++ src/app/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.sass';
+
+const Header = (props) => {
+  const user = localStorage.getItem('currentUser')
+  return (
+    <header>
+      <h1>Liverpool App</h1>
+      <div className="buttons">
+        {
+          user
+          ? (
+            <Link className="logout" onClick={props.logout} to='/'>
+              <i className="fas fa-lock"></i>
+              Logout
+            </Link>
+          )
+          : ''
+        }
+      </div>
+    </header>
+  )
+}
+
+export default Header
